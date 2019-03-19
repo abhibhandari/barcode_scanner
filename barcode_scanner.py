2b8c722378de6d2c0fc7fed68868bb89a952a4d8
Abhishek Bhandari <abhishek.bhandari@atos.net>
2019-03-19 10:53:59 +0530

    added sample screenshot

--- barcode_scanner.py
+++ barcode_scanner.py
@@ -4,6 +4,7 @@ from pyzbar import pyzbar
 import argparse
 import glob
 from PIL import Image 
+#import cv2
 
 # construct the argument parser and parse the arguments
 # ap = argparse.ArgumentParser()
@@ -11,35 +12,35 @@ from PIL import Image
 # 	help="path to input image")
 # args = vars(ap.parse_args())
 
-# load the input image
+# # load the input image
 # image = cv2.imread(args["image"])
-# image = Image.open(args["image"])
+#image = Image.open(args["image"])
 # print("Image ", image.filename)
 
-# find the barcodes in the image and decode each of the barcodes
+# # find the barcodes in the image and decode each of the barcodes
 # barcodes = pyzbar.decode(image)
 
-# # loop over the detected barcodes
+# loop over the detected barcodes
 # for barcode in barcodes:
-	# extract the bounding box location of the barcode and draw the
-	# bounding box surrounding the barcode on the image
-	# (x, y, w, h) = barcode.rect
-	# cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# 	# extract the bounding box location of the barcode and draw the
+# 	# bounding box surrounding the barcode on the image
+# 	(x, y, w, h) = barcode.rect
+# 	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
  
-	# the barcode data is a bytes object so if we want to draw it on
-	# our output image we need to convert it to a string first
-	# barcodeData = barcode.data.decode("utf-8")
-	# barcodeType = barcode.type
+# 	# the barcode data is a bytes object so if we want to draw it on
+# 	# our output image we need to convert it to a string first
+# 	barcodeData = barcode.data.decode("utf-8")
+# 	barcodeType = barcode.type
  
-	# # draw the barcode data and barcode type on the image
-	# # text = "{} ({})".format(barcodeData, barcodeType)
-	# # cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-	# # 	0.5, (0, 0, 255), 2)
+# 	# draw the barcode data and barcode type on the image
+# 	text = "{} ({})".format(barcodeData, barcodeType)
+# 	cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+# 		0.5, (0, 0, 255), 2)
  
-	# # print the barcode type and data to the terminal
-	# print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+# 	# print the barcode type and data to the terminal
+# 	print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
  
-# show the output image
+# # show the output image
 # cv2.imshow("Image", image)
 # cv2.waitKey(0)
 
